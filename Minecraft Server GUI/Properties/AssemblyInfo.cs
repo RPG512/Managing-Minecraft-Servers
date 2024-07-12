b06f6b6ac8443590a49a3500e30a9212c7e8b3e5
RPG512 <maxim-kotenko@hotmail.com>
2024-07-12 13:36:25 +0300

    м AssemblyInfo...

---
+++ Minecraft Server GUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("0.1.5.13")]
+[assembly: AssemblyProduct("Managing Minecraft Servers")]
+[assembly: AssemblyCompany("512Studio")]
+[assembly: AssemblyTrademark("512Studio")]
+[assembly: AssemblyCopyright("Copyright © 512Studio 2024")]
+[assembly: AssemblyTitle("Minecraft Server GUI")]
+
+// В проектах SDK, таких как этот, некоторые атрибуты сборки, которые ранее определялись
+// в этом файле, теперь автоматически добавляются во время сборки и заполняются значениями,
+// заданными в свойствах проекта. Подробные сведения о том, какие атрибуты включены
+// и как настроить этот процесс, см. на странице: https://aka.ms/assembly-info-properties.
+
+
+// При установке значения false для параметра ComVisible типы в этой сборке становятся
+// невидимыми для компонентов COM. Если вам необходимо получить доступ к типу в этой
+// сборке из модели COM, установите значение true для атрибута ComVisible этого типа.
+
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов typelib, если этот проект
+// будет видимым для COM.
+
+[assembly: Guid("d992c6bf-c5a5-4245-b287-7f4754c66b1d")]
